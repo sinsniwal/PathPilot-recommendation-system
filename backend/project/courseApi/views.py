88b29000885d84ac3f2f0f30fe3b3bf2e58fec43
Dhairya-Arora01 <dhairyarora0208in@gmail.com>
2023-11-16 12:50:36 +0530

    Recommendation endpoint done

--- backend/project/courseApi/views.py
+++ backend/project/courseApi/views.py
@@ -1,10 +1,13 @@
 from .utils import Model
+from authApi.models import Student
+from rest_framework import status
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
 
-
+#TODO: create tests for getRoadmap()
 def getRoadmap(courses_completed: list):
     """
     getRoadmap returns the roadmap given the completed courses
@@ -16,12 +19,24 @@ def getRoadmap(courses_completed: list):
 
 class CourseAPI(APIView):
     def get(self, request):
+        a = JWTAuthentication().authenticate(request) 
+        user = a[0]
 
-        token = request.headers.get('Bearer')     #no validation till now for token  
-        roadmap = getRoadmap([])
+        try:
+            student_user = Student.objects.get(user = user)
+        except:
+            return Response({
+                "message": "You must be a student to get recommendation!",
+                "status": status.HTTP_400_BAD_REQUEST
+            })
+
+        completed_courses = student_user.completed_courses.all()
+        completed_courses = [course.course_code for course in completed_courses]
+
+        roadmap = getRoadmap(completed_courses)
 
         return Response({
-            "message": token,
+            "completed_courses": completed_courses,
             "roadmap": roadmap
         })
 
