15f1374375721183ecdf84ce19b77226ce376fe7
Dhairya-Arora01 <dhairyarora0208in@gmail.com>
2023-12-07 13:13:51 +0530

    Added bootstrap for vue and created navbar

--- frontend/project/src/App.vue
+++ frontend/project/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar></Navbar>
+  <HelloWorldVue></HelloWorldVue>
+  <button type="button" class="btn btn-warning">Warning</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navbar from './components/Navbar.vue';
+import HelloWorldVue from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    HelloWorldVue
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
